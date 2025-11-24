6385bafaa64edfff73acd55a682686d089c96ca8
Kalina Baniak <kalina.baniak@student.pk.edu.pl>
2025-11-24 22:48:53 +0100

    feat(auth): add routes for login and protected profile

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import login, profile
 
 urlpatterns = [
     path('users/', views.get_users),
     path('users/<int:id>/', views.get_user_by_id),
+    path("login/", login),
+    path("profile/", profile),
 ]
