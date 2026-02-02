d1f134b81c2083c8cccfeba171c1e81cbdb65e64
Kalina Baniak <kalina.baniak@student.pk.edu.pl>
2026-02-02 20:36:30 +0100

    feat(auth): enable JWT token blacklist support

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api_app'
+    'api_app',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
