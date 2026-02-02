49bcfea6c184e02eebdd5373dac8c1a685c3e46a
Kalina Baniak <kalina.baniak@student.pk.edu.pl>
2026-02-02 20:37:32 +0100

    feat(auth): add logout endpoint invalidating refresh tokens

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenBlacklistView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_app.urls')),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
 ]
