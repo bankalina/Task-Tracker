5d690d6128e2d5e95eddab1f7fda00394eccda77
Kalina Baniak <kalina.baniak@student.pk.edu.pl>
2026-02-02 23:30:05 +0100

    feat(subtasks): add subtask serializer

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Subtask
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -9,3 +9,10 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = "__all__"
         read_only_fields = ("created_at", "updated_at")
+
+
+class SubtaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subtask
+        fields = "__all__"
+        read_only_fields = ("created_at", "updated_at", "task")
