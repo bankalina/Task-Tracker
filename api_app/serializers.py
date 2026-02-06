43a44cf393381450321d2a69b023f4eab21baa17
Kalina Baniak <kalina.baniak@student.pk.edu.pl>
2026-02-06 23:43:03 +0100

    feat(rbac): add membership serializer

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Task, Subtask
+from .models import Task, Subtask, UserTask
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -25,4 +25,26 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "email"]
+
+
+class UserTaskSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        source="user",
+        write_only=True,
+        required=False,
+    )
+
+    class Meta:
+        model = UserTask
+        fields = ["id", "task", "user", "user_id", "role"]
+        read_only_fields = ("id", "task", "user")
+
+    def validate(self, attrs):
+        if self.instance is None and "user" not in attrs:
+            raise serializers.ValidationError("user_id is required.")
+        if self.instance is not None and "user" in attrs:
+            raise serializers.ValidationError("Changing user is not allowed.")
+        return attrs
         
